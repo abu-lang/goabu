88db22ccad46237e82f5dfe96531cb07c6c900c8
Massimo Comuzzo <comuzzo.massimo@spes.uniud.it>
2023-02-04 22:06:30 +0100

    Allow setting of debug mode through GOABU_DEBUG environment variable
    Squashed commit of the following:
    
    commit eb13d48a95628d8a1d439bce29169e3e3eda2743
    Author: Massimo Comuzzo <comuzzo.massimo@spes.uniud.it>
    Date:   Sat Feb 4 22:03:48 2023 +0100
    
        Revert "Add prepare-release.sh script"
    
        This reverts commit 0b41618540dcb01f268341773a3aee54e3a6dcbf.
    
    commit 49b0973b19b528b3e54ae97a29e1598bd6860a98
    Author: Massimo Comuzzo <comuzzo.massimo@spes.uniud.it>
    Date:   Sat Feb 4 22:03:16 2023 +0100
    
        Revert "Simplify config/productionFlags.go's merge conflicts"
    
        This reverts commit a83437c9b2c6462f1db16919c3258ed6ea5013fa.
    
    commit 9c6b30e2a4b34ba0c41542dfb6a4a37819e064c0
    Author: Massimo Comuzzo <comuzzo.massimo@spes.uniud.it>
    Date:   Sat Feb 4 21:55:43 2023 +0100
    
        Allow setting of debug mode through GOABU_DEBUG environment variable

--- config/productionFlags.go
+++ config/productionFlags.go
@@ -4,4 +4,6 @@
 // Package config contains GoAbU's global configuration.
 package config
 
-var Production = false // v0.3.0 (development)
+import "os"
+
+var Production = os.Getenv("GOABU_DEBUG") != "1"
